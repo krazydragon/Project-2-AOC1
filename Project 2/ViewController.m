e3c6a6f2ceffe6fcb47f3e468a22ae3423ecb41a
Ronaldo Barnes <krazydragon@fullsail.edu>
2012-06-07 18:29:21 -0700

    finished adding list of items and clean up.

--- Project 2/ViewController.m
+++ Project 2/ViewController.m
@@ -16,6 +16,8 @@
 
 - (void)viewDidLoad
 {
+    //Background color
+    self.view.backgroundColor = [UIColor blackColor];
     
     //Book Title   
     bookTitle = [[UILabel alloc] initWithFrame:CGRectMake(0.0f, 0.0f, 320.0f, 20.0f)];
@@ -28,6 +30,7 @@
     }
     
     [self.view addSubview:bookTitle];
+    
     //Author info
     author = [[UILabel alloc] initWithFrame:CGRectMake(0.0f, 50.0f, 100.0f, 20.0f)];
     if (author != nil)
@@ -72,7 +75,7 @@
     //Summary
     
     summary = [[UILabel alloc] initWithFrame:CGRectMake(0.0f, 110.0f, 100.0f, 20.0f)];
-    if (publishedDate != nil)
+    if (summary != nil)
     {
         summary.backgroundColor = [UIColor redColor];
         summary.text = @"Summary";
@@ -82,7 +85,7 @@
     [self.view addSubview:summary];
     
     summaryText = [[UILabel alloc] initWithFrame:CGRectMake(0.0f, 130.0f, 320.0f, 250.0f)];
-    if (publishedDate != nil)
+    if (summaryText != nil)
     {
         summaryText.backgroundColor = [UIColor magentaColor];
         summaryText.text = @"This book is the first of a series of books and a TV series on HBO True Blood. It all starts with Sookie Stackhouse from Bon Temps Louisiana. She is 25 years old and a mind reader. Most of the people from her very small town think that she is slow but she tries not to get involved with other people since she can read minds. This all changes when she meets a Vampire named Bill and she can’t read his mind the two hit it off. ";
@@ -101,12 +104,39 @@
     item4 = [[NSString alloc] initWithString:@"New age"];
     item5 = [[NSString alloc] initWithString:@"Violence"];
     //Create Array
-    itemArray = [[NSArray alloc] initWithObjects:item1,item2,item3,item4,item5,nil];
+    itemArray = [NSArray arrayWithObjects:item1, item2, item3, item4, item5, nil];
+    //NSLog(@"%@", [itemArray description]);
+    
     //Create Mutible String
+    itemText = [[NSMutableString alloc] initWithCapacity:5];
     
+    for (int x = 0; x < itemArray.count; x++) {
+        [itemText appendString:[itemArray objectAtIndex: x]];
+        if (x < itemArray.count -1) {
+            [itemText appendString: @", "];
+        }
+    }    
     
+    itemList = [[UILabel alloc] initWithFrame:CGRectMake(0.0f, 400.0f, 100.0f, 20.0f)];
+    if (itemList != nil)
+    {
+        itemList.backgroundColor = [UIColor orangeColor];
+        itemList.text = @"List of items";
+        itemList.textAlignment = UITextAlignmentLeft;
+        itemList.textColor = [UIColor whiteColor];
+    }
+    [self.view addSubview:itemList];
     
-    
+    itemListText = [[UILabel alloc] initWithFrame:CGRectMake(0.0f, 420.0f, 320.0f, 40.0f)];
+    if (itemListText != nil)
+    {
+        itemListText.backgroundColor = [UIColor darkGrayColor];
+        itemListText.text = itemText;
+        itemListText.textAlignment = UITextAlignmentCenter;
+        itemListText.textColor = [UIColor yellowColor];
+        itemListText.numberOfLines = 2;
+    }
+    [self.view addSubview:itemListText];
     
     
     
